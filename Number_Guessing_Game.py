2d869bcf0804ba0f5f920280ab88994ace99d7ab
Sooryakrishna M <sk2004international@gmail.com>
2024-09-16 11:54:57 +0530

    the finalization

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,7 +1,7 @@
 import random
 
 top_of_range=input("Type a number :")
-
+guesses=0
 if top_of_range.isdigit():
     top_of_range=int(top_of_range)
 
@@ -15,3 +15,20 @@ else:
 
 random_number=random.randint(0,top_of_range)
 print(random_number)
+
+while True:
+    guesses+=1
+    user_input=input("Make a guess")
+    if user_input.isdigit():
+        user_input=int(user_input)
+    else:
+        print("Please type a number next time")
+        continue
+    if user_input == random_number:
+        print("You got it correct!")
+        break
+    elif user_input>random_number:
+        print("You were above the number")
+    else:
+        print("You were below the number")
+print("You got it in",guesses,"guesses")
